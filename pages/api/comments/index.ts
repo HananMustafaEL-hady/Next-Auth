5421285e7617a0d0e66ee9eed3db0605a3693954
HananMustafaEL-hady <hananmustafa.hassan@gmail.com>
2021-11-20 13:31:37 +0200

    API POST Request

--- pages/api/comments/index.ts
+++ pages/api/comments/index.ts
@@ -1,6 +1,28 @@
+import { comments } from './../../../data/comments';
 import type { NextApiRequest, NextApiResponse } from 'next'
-import {comments} from "../../../data/comments"
 
 export default function handler(req: NextApiRequest,res: NextApiResponse) {
-  res.status(200).json({comments})
+ 
+    if(req.method=='GET'){
+        res.status(200).json({comments})
+
+    }
+    else if(req.method==='POST'){
+const comment=JSON.parse(req.body).comment
+if(comment){
+    const newComment={
+        id:Date.now(),
+        text:comment
+      }
+      comments.push(newComment)
+      res.status(200).json(newComment)
+}else {
+    res.status(404).json({message:"comment is required"})
+
 }
+
+
+    }
+
+}
+
--- pages/comments/index.tsx
+++ pages/comments/index.tsx
@@ -1,19 +1,32 @@
 import React, { useState } from 'react'
 import { CommentsType } from '../../data/comments'
 const Comments = () => {
-
-
     const [comments, setComments] = useState<CommentsType[]>()
-
+    const [comment, setComment] = useState('')
     const fetchComments = async () => {
         const response = await fetch('/api/comments')
         const data = await response.json()
         setComments(data.comments)
     }
+    const submitComment = async () => {
+
+        const response = await fetch('/api/comments', {
+            method: 'POST',
+            body: JSON.stringify({ comment }),
+            headers: {
+                'Content-Type': 'aplication/json'
+            }
+        })
+
+        const data = await response.json();
+        console.log(data)
+    }
 
     return (
 
         <div>
+            <input type="text" value={comment} onChange={e => setComment(e.target.value)} />
+            <button onClick={submitComment}>Submit Comment</button>
             {comments && comments?.map(item => {
                 return (<div key={item.id}>
                     {item.id} {item.text}
